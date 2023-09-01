b92931366632ca08c8a4e6022a5c5847440ad29a
EddyPBR <edvaldojunior1310@gmail.com>
2023-08-31 22:27:38 -0300

    :wrench: (api) config api to get .env data

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,19 @@
+import dotenv from "dotenv";
+
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "./modules/config/config.service";
 
 async function bootstrap() {
+  dotenv.config({ path: ".env.development" });
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3333);
+
+  const configService = app.get(ConfigService);
+
+  const { API_PORT } = configService.getEnvVars();
+
+  await app.listen(API_PORT);
 }
 
 bootstrap();
